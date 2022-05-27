78c8538987798df7cf82b1f2e165190d5614a75e
Matheus Antonio <matheusantonio232@gmail.com>
2022-05-26 22:14:13 -0300

    Iniciando projeto spring boot

---
+++ src/main/java/com/healthcare/requestdonatemedicine/RequestDonateMedicineApplication.java
@@ -0,0 +1,13 @@
+package com.healthcare.requestdonatemedicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestDonateMedicineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestDonateMedicineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthcare/requestdonatemedicine/RequestDonateMedicineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.requestdonatemedicine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestDonateMedicineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
