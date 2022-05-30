f9b013ddc8354231a428d531893e016ac4885212
Matheus Antonio <matheusantonio232@gmail.com>
2022-05-29 21:43:42 -0300

    controller get login

---
+++ src/main/java/com/healthcare/requestdonatemedicine/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.healthcare.requestdonatemedicine.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+  @GetMapping(value = "/login")
+  public String getLoginPage() {
+    return "login";
+  }
+
+  @PostMapping(value = "login")
+  public String login(@RequestParam String username, @RequestParam String password) {
+    System.out.println(username + password);
+    return "/login";
+  }
+
+}
