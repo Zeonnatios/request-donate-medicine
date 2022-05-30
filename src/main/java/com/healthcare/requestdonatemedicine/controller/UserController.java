3278094374cd6578218efd07ae1199de0eee8a3a
Matheus Antonio <matheusantonio232@gmail.com>
2022-05-29 23:15:03 -0300

    add register route

--- src/main/java/com/healthcare/requestdonatemedicine/controller/UserController.java
+++ src/main/java/com/healthcare/requestdonatemedicine/controller/UserController.java
@@ -1,5 +1,14 @@
 package com.healthcare.requestdonatemedicine.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+  @GetMapping(value = "/register")
+  public String getRegisterPage() {
+    return "register";
+  }
 
 }
