e49c9f4138653591d8888f177946e243b7a819ec
Matheus Antonio <matheusantonio232@gmail.com>
2022-06-06 13:17:29 -0300

    implementing userservice

---
+++ src/main/java/com/healthcare/requestdonatemedicine/model/services/UserService.java
@@ -0,0 +1,11 @@
+package com.healthcare.requestdonatemedicine.model.services;
+
+import com.healthcare.requestdonatemedicine.model.entities.User;
+
+public interface  UserService {
+
+  public void saveUser(User user);
+
+  public User findUserByUsername(String username);
+
+}
---
+++ src/main/java/com/healthcare/requestdonatemedicine/model/services/UserServiceImp.java
@@ -0,0 +1,23 @@
+package com.healthcare.requestdonatemedicine.model.services;
+
+import com.healthcare.requestdonatemedicine.model.entities.User;
+import com.healthcare.requestdonatemedicine.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImp implements UserService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Override
+  public void saveUser(User user) {
+    userRepository.save(user);
+  }
+
+  @Override
+  public User findUserByUsername(String username) {
+    return userRepository.findByUsername(username);
+  }
+}
