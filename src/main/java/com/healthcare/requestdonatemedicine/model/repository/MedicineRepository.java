6aca604818b0e35133c86c5272facd8a1b5da034
Matheus Antonio <matheusantonio232@gmail.com>
2022-06-06 19:26:41 -0300

    feat: update medicine repository

--- src/main/java/com/healthcare/requestdonatemedicine/model/repository/MedicineRepository.java
+++ src/main/java/com/healthcare/requestdonatemedicine/model/repository/MedicineRepository.java
@@ -2,7 +2,9 @@ package com.healthcare.requestdonatemedicine.model.repository;
 
 import com.healthcare.requestdonatemedicine.model.entities.Medicine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MedicineRepository extends JpaRepository<Medicine, Integer> {
 
 }
