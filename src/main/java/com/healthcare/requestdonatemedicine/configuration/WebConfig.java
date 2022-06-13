4da3490a8ffd9b1ff7c8d5907c51ff5359aae15e
Matheus Antonio <matheusantonio232@gmail.com>
2022-06-13 15:04:48 -0300

    feat: add webappinitializer and webconfig

--- src/main/java/com/healthcare/requestdonatemedicine/configuration/WebConfig.java
+++ src/main/java/com/healthcare/requestdonatemedicine/configuration/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @EnableWebMvc
 @Configuration
@@ -14,6 +15,9 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
+    registry.addResourceHandler("/resources/**")
+        .addResourceLocations("/resources/").setCachePeriod(3600)
+        .resourceChain(true).addResolver(new PathResourceResolver());
 
   }
 
